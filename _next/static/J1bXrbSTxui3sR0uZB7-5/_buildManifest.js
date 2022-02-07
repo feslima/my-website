9c5f3153b0b06481b840547884b7f8e1c2a98e60
feslima <feslima@users.noreply.github.com>
2022-02-07 20:14:02 +0000

    deploy: 7ae3f974f3793053a6d6e60731de8b97b0362b19

---
+++ _next/static/J1bXrbSTxui3sR0uZB7-5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/230-2e77fd8b13d3ce4f.js","static/chunks/pages/index-7e763d71ca5ed747.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
