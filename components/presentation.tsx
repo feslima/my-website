ef3b0d87f97f468d70fb26ac19d324fc1d8e34e1
Felipe <feslima93@gmail.com>
2022-02-08 16:52:22 -0300

    I18n: Presentation section

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -7,15 +7,9 @@ const Presentation: FunctionComponent<{}> = () => {
 
   return (
     <Section id="presentation">
-      <SectionTitle text={t('greeting')} animate={true} />
-      <p>
-        If you got here I'm assuming you're not lost. Even if you are, maybe
-        this could still be productive to you.
-      </p>
-      <p>
-        Anyways, I hope you like it here since this is what I consider the
-        entrypoint for my work showcases.
-      </p>
+      <SectionTitle text={t('presentation.greeting')} animate={true} />
+      <p>{t('presentation.first')}</p>
+      <p>{t('presentation.second')}</p>
     </Section>
   );
 };
